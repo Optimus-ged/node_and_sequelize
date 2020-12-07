0350972cd89f4d48773acbaf1487dffa54c665b5
Optimus-ged <gedeonyalakuhanda@gmail.com>
2020-12-07 17:39:42 +0200

    adding controller for product model

---
+++ controller/product_controller.js
@@ -0,0 +1,54 @@
+// Comment
+// Import dependencies
+import Product from '../models/products';
+
+const productController = {
+    // Comment
+    // Get-all products controller
+    getProducts: async (req, res) => {
+        let response = await Product.findAll().then().catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: err.message
+                }
+            });
+        });
+        res.status(200).json({
+            products: response
+        });
+    },
+
+    // Comment
+    // add-products controller
+    addProduct: async (req, res) => {
+        let data = {
+            name: req.body.name,
+            price: NaN(parseFloat(req.body.price)) ? 0 : parseFloat(req.body.price)
+        };
+        let response = await Product.create({
+            name: data.name,
+            price: data.price
+        }).then().catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: err.message
+                }
+            });
+        });
+        if (response) {
+            return res.status(200).json({
+                message: 'Sucessfully created',
+                data: response
+            });
+        }
+        res.status(500).json({
+            error: {
+                message: 'Not created'
+            }
+        });
+    },
+};
+
+export default productController;
