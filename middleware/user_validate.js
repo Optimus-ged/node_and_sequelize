4563f35c0ebdde7ab73418acd56c46e568060c59
Optimus-ged <gedeonyalakuhanda@gmail.com>
2020-12-11 23:41:33 +0200

    handling user signup middleware

---
+++ middleware/user_validate.js
@@ -0,0 +1,26 @@
+// Comment
+// Importing dependencies
+import Joi from 'joi';
+
+// Comment
+// user validation
+const userValidation = {
+    userValidate : async(req, res, next)=>{
+        let schema = Joi.object({
+            email : Joi.string().min(5).required(),
+            password : Joi.string().min(5).required()
+        });
+        let { error } =schema.validate(req.body);
+        if(error){
+            return res.status(400).json({
+                status : 400,
+                message : error.details[0].message
+            });
+        }
+        next();
+    }
+};
+
+// Comment
+// Exporting module
+export default userValidation;
--- routers/user_routers.js
+++ routers/user_routers.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import userController from '../controller/user_controller';
 import checkAuth from '../middleware/token';
+import userValidation from '../middleware/user_validate';
 
 // Comment
 // Declaring variables
@@ -14,7 +15,7 @@ router
     .get('/', checkAuth, userController.getUsers)
     .get('/:id', checkAuth, userController.getOneUser)
     .post('/login', userController.loginUser)
-    .post('/signup', checkAuth, userController.addUser)
+    .post('/signup', userValidation.userValidate, checkAuth, userController.addUser)
     .put('/:id', checkAuth, userController.updateUser)
     .delete('/:id',checkAuth, userController.deleteUser)
 // Comment
