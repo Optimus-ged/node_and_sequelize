d09a4a5c32ce500541bc5f298dd282b11b0fce77
Optimus-ged <gedeonyalakuhanda@gmail.com>
2020-12-11 23:47:46 +0200

    fixing some errors at product validation middleware

--- middleware/product_validate.js
+++ middleware/product_validate.js
@@ -11,21 +11,16 @@ const productvalidation = {
             price : Joi.number().precision(2).required()
         });
         let { error } = schema.validate(req.body);
-        errorFunction(error, res, next);
+        if(error){
+            return res.status(400).json({
+                status : 400,
+                message : error.details[0].message
+            });
+        }
+        next();
     }
 };
 
-// Comment
-// function that handling errors
-function errorFunction(error, res, next) {
-    if(error){
-        return res.status(400).json({
-            status : 400,
-            message : error.details[0].message
-        });
-    }
-    next();
-}
 
 // Comment
 // Exporting modules
