2d818f7d7c32e4c428ea17dd031564e604bf1348
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-13 14:04:37 +0200

    repetition on delete product endpoint for more comprehension

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -1,144 +1,143 @@
 // Comment
 // Import dependencies
-import Product from '../models/product_model';
+import Product from "../models/product_model";
 
 // Comment
 // The product controller
 const productController = {
-    // Comment
-    // Get-all products controller
-    getProducts: async (req, res) => {
-        let response = await Product.findAll().catch(err => { console.log(err); });
+  // Comment
+  // Get-all products controller
+  getProducts: async (req, res) => {
+    let response = await Product.findAll().catch((err) => {
+      console.log(err);
+    });
+    res.status(200).json({
+      status: 200,
+      message: "All products successfully getted",
+      count: response.length,
+      products: response,
+    });
+  },
+
+  // Comment
+  // Get product by id controller
+  getOneProduit: async (req, res) => {
+    let id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
+    let response = await Product.findOne({ where: id });
+    if (response) {
+      return res.status(200).json({
+        status: 200,
+        message: "Product successfully getted",
+        product: response,
+      });
+    }
+    res.status(401).json({
+      error: {
+        message: "product not found",
+      },
+    });
+  },
+
+  // Comment
+  // Add product
+  addProduct: async (req, res) => {
+    let data = {
+      name: req.body.name,
+      price: isNaN(parseFloat(req.body.price)) ? 0 : parseFloat(req.body.price),
+    };
+
+    let response = await Product.create({
+      name: data.name,
+      price: data.price,
+    }).catch((err) => {
+      console.error(err);
+    });
+
+    if (response) {
+      res.status(201).json({
+        status: 201,
+        message: "Product Successfully created",
+        response: response,
+      });
+    }
+  },
+
+  // Comment
+  // Adding many products at once
+  addProducts: async (req, res) => {
+    let data = req.body;
+    let response = [];
+
+    for (let i = 0; i < data.length; i++) {
+      response.push(
+        await Product.create({
+          name: data[i].name,
+          price: data[i].price,
+        })
+      );
+    }
+
+    if (response.length > 0) {
+      res.status(200).json({
+        status: 200,
+        message: "All products success created",
+        response: response,
+      });
+    }
+  },
+
+  // Comment
+  // Update a product
+  updateProduct: async (req, res) => {
+    let _id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
+    let response = await Product.findOne({ where: { id: _id } });
+    if (!response) {
+      res.status(404).json({
+        status: 404,
+        message: "Product not found",
+      });
+    }
+    if (response) {
+      let updated = await Product.update(
+        {
+          name: req.body.name,
+          price: req.body.price,
+        },
+        { where: { id: _id } }
+      );
+      if (updated[0] != 0) {
         res.status(200).json({
-            status: 200,
-            message: 'All products successfully getted',
-            count: response.length,
-            products: response
-            
+          status: 200,
+          message: "Product updated succesfully",
         });
-    },
-
-    // Comment
-    // Get product by id controller
-    getOneProduit: async (req, res) => {
-        let id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
-        let response = await Product.findOne({ where: id })
-        if (response) {
-            return res.status(200).json({
-                status: 200,
-                message: 'Product successfully getted',
-                product: response
-            });
-        }
-        res.status(401).json({
-            error: {
-                message: 'product not found'
-            }
+      } else {
+        res.status(500).json({
+          status: 500,
+          message: "Product not updated",
         });
-    },
+      }
+    }
+  },
 
-    // Comment
-    // Add product
-    addProduct: async (req, res) => {
-        let data = {
-            name: req.body.name,
-            price: isNaN(parseFloat(req.body.price)) ? 0 : parseFloat(req.body.price)
-        };
-
-        let response = await Product.create({
-            name: data.name,
-            price: data.price
-        }).catch(err => { console.error(err); });
-
-        if (response) {
-            res.status(201).json({
-                status: 201,
-                message: 'Product Successfully created',
-                response: response
-            });
-        }
-    },
-
-
-    // Comment
-    // Adding many products at once
-    addProducts: async (req, res) => {
-        let data = req.body;
-        let response = [];
-
-        for (let i = 0; i < data.length; i++) {
-            response.push(
-                await Product.create({
-                    name: data[i].name,
-                    price: data[i].price
-                })
-            )
-        }
-
-        if (response.length > 0) {
-            res.status(200).json({
-                status: 200,
-                message: 'All products success created',
-                response: response
-            });
-        }
-    },
-
-    // Comment
-    // Update a product
-    updateProduct: async (req, res) => {
-        let _id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
-        let response = await Product.findOne({ where: { id: _id } });
-        if (!response) {
-            res.status(404).json({
-                status: 404,
-                message: 'Product not found',
-            });
-        }
-        if (response) {
-            let updated = await Product.update(
-                {
-                    name: req.body.name,
-                    price: req.body.price
-                },
-                { where: { id: _id } }
-            );
-            if (updated[0] != 0) {
-                res.status(200).json({
-                    status: 200,
-                    message: 'Product updated succesfully'
-                });
-            } else {
-                res.status(500).json({
-                    status: 500,
-                    message: 'Product not updated'
-                });
-            }
-        }
-    },
-
-    // Comment
-    // Delete a product
-    deleteProduct: async (req, res) => {
-        let _id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
-        let response = await Product.findOne({ where: { id: _id } });
-        if (!response) {
-            res.status(404).json({
-                status: 404,
-                message: 'Product does not exist'
-            });
-        } else {
-            await Product.destroy({ where: { id: _id } });
-            res.status(200).json({
-                status: 200,
-                data: {
-                    message: 'Product deleted Successfully'
-                }
-            });
-        }
-    },
+  // Comment
+  // Delete a product
+  deleteProduct: async (req, res) => {
+    let _id = isNaN(parseInt(req.params.id)) ? 0 : parseInt(req.params.id);
+    let response = await Product.findOne({ where: { id: _id } });
+    if (!response) {
+      res.status(404).json({
+        status: 404,
+        message: "This product does not exist",
+      });
+    } else {
+      await Product.destroy({ where: { id: _id } });
+      res.status(200).json({
+        status: 200,
+        message: "Product successfully deleted",
+      });
+    }
+  },
 };
-
 
 // Comment
 // Exporting module
