068438bc190086194abe76af68f140a44ac36444
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-22 21:39:05 +0200

    an other repetition on product validation middleware

--- middleware/product_validate.js
+++ middleware/product_validate.js
@@ -1,27 +1,26 @@
 // Comment
 // Importing dependencies
-import Joi from 'joi';
+import Joi from "joi";
 
 // Comment
 // Product validation with joi dependency
 const productvalidation = {
-    validatedProduct: async (req, res, next) => {
-        let schema = Joi.object({
-            name: Joi.string().min(6).max(50).required(),
-            price: Joi.number().precision(2).required()
-        });
+  validatedProduct: async (req, res, next) => {
+    let schema = Joi.object({
+      name: Joi.string().min(6).max(50).required(),
+      price: Joi.number().precision(2).required(),
+    });
 
-        let { error } = schema.validate(req.body);
-        if (error) {
-            return res.status(400).json({
-                status: 400,
-                message: error.details[0].message
-            });
-        }
-        next();
+    let { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        status: 400,
+        message: error.details[0].message,
+      });
     }
+    next();
+  },
 };
-
 
 // Comment
 // Exporting modules
--- middleware/user_validate.js
+++ middleware/user_validate.js
@@ -1,26 +1,26 @@
 // Comment
-// Import dependancy
-import Joi from 'joi';
+// Importing dependencies
+import Joi from "joi";
 
 // Comment
-// User validation
-const UserValidation = {
-    UserValidate = async (req, res, next)=> {
-        let schema = Joi.object({
-            email : Joi.string().min(5).required(),
-            password : Joi.string().min(5).required()
-        });
-        const {error} = schema.validate(req.body);
-        if(error){
-            return res.status(500).json({
-                status : 500,
-                message : error.details[0].message
-            });
-        }
-        next();
+// user validation
+const userValidation = {
+  userValidate: async (req, res, next) => {
+    let schema = Joi.object({
+      email: Joi.string().min(5).required(),
+      password: Joi.string().min(5).required(),
+    });
+    let { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        status: 400,
+        message: error.details[0].message,
+      });
     }
+    next();
+  },
 };
 
 // Comment
 // Exporting module
-export default UserValidation;
+export default userValidation;
