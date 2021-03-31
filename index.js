78640edcfe49eff879404780f0eb96658c950a11
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-01 00:20:39 +0300

    An other repeat made on main file

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-// Importing dependancies
-import express from "express";
-import dotenv from "dotenv";
-import database from "./config/database";
-import allRoutes from "./routers/principle/routers_index";
+// Import all importants dependancies
+import express from 'express';
+import dotenv from 'dotenv';
+import database from './config/database';
+import allRoutes from './routers/principle/routers_index';
 
-// Configuring dotenv
+// Config dotenv
 dotenv.config();
 
-// Configuring environment variables
+// Creating some importants constants
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Formatting data
+// Formating data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
-// Testing database connection
-const testConnection = async () => {
+// Config all routes
+app.use("/api", allRoutes);
+
+// Config database
+const testConnection = async(req, res)=>{
   try {
     database.authenticate();
-    console.log("The conection to the database is OK !!!");
+    console.log("Database connection is Ok !!!");
   } catch (error) {
     console.error(error);
   }
 };
 testConnection();
 
-// Config all routes
-app.use("/api", allRoutes);
-
-// All unknown routes
-app.use("**", (req, res) => {
-  req.status(404).json({
-    status: 404,
-    message: "Request not found on the Server",
+// Catching all unknown endpoints
+app.use("**", (req, res)=>{
+  res.status(404).json({
+    status : 404,
+    message : "Request was not found on the server"
   });
 });
 
-// Welcome visiters of our endpoint
-app.get("/", (res, res) => {
+// Welcome guests on our web server
+app.get("/", (req, res)=>{
   res.status(200).json({
-    status: 200,
-    message: "Welcome to Optimus-ged API",
+    status : 200,
+    message : "Welcome to Optimus ged API !!!"
   });
 });
 
-// Listening port
-app.listen(port, () => {
-  console.log(`The server is running at port ${port}`);
-});
+// Endpoint listening to the port
+app.listen(port, ()=>console.log(`The server is running at port ${port}`));
