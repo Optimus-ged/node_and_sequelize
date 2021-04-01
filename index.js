ce279e1267ede526575c70c95b2974dca3e9e7f2
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-01 12:16:09 +0300

    Fixing errors in index file

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
-// Import all importants dependancies
-import express from 'express';
-import dotenv from 'dotenv';
-import database from './config/database';
-import allRoutes from './routers/principle/routers_index';
+// Importing dependancies and others
+import express from "express";
+import dotenv from "dotenv";
+import database from "./config/database";
+import allRoutes from "./routers/principle/routers_index";
 
 // Config dotenv
 dotenv.config();
 
-// Creating some importants constants
+// Config constants variables
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Formating data
+// Config data formatting
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-// Config all routes
+// Conig middlewares
 app.use("/api", allRoutes);
 
+// Welcome guest to our web server
+app.use("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Welcome to Optimus ged Api",
+  });
+});
+
+// Handling errors when endpoint doesn't match
+app.use("**", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: {
+      message: "The request was not found on the Server",
+    },
+  });
+});
+
 // Config database
-const testConnection = async(req, res)=>{
+const testConnection = async () => {
   try {
     database.authenticate();
-    console.log("Database connection is Ok !!!");
+    console.log("The connection to the database is Ok !!!");
   } catch (error) {
     console.error(error);
   }
 };
 testConnection();
 
-// Catching all unknown endpoints
-app.use("**", (req, res)=>{
-  res.status(404).json({
-    status : 404,
-    message : "Request was not found on the server"
-  });
-});
-
-// Welcome guests on our web server
-app.get("/", (req, res)=>{
-  res.status(200).json({
-    status : 200,
-    message : "Welcome to Optimus ged API !!!"
-  });
-});
-
-// Endpoint listening to the port
-app.listen(port, ()=>console.log(`The server is running at port ${port}`));
+// Config port on with app is listening
+app.listen(port, () => console.log(`The Server is running at port ${port}`));
