683c428be174f3a9b59aa7b86480845fa04d9234
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-15 12:36:04 +0300

    Poste model created successfully

---
+++ models/post_model.js
@@ -0,0 +1,27 @@
+// Importing dependancies
+import sequelize from "sequelize";
+import database from "../config/database" ;
+
+// Building poste agent model
+const poste = database.define(
+    "poste",
+    {
+        id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true,
+            allowNull : false
+        },
+        designation : {
+            type : DataTypes.STRING,
+            allowNull : false
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    }
+);
+
+// Exporting poste model
+export default poste
