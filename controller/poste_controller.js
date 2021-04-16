342a34a6357b8a876ce3bd190c614f5985c4bc6f
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-16 14:43:20 +0300

    Include handeled in poste controller

--- controller/poste_controller.js
+++ controller/poste_controller.js
@@ -1,18 +1,12 @@
 // Import model
 import Poste from "../models/poste_model";
 import agent from "../models/agent_model";
-import { Op } from "sequelize";
 
 // Building components for all operations
 const posteController = {
   // Getting all data from poste table
   getPosts: async (req, res) => {
     let response = await Poste.findAll({
-      where: {
-        id: {
-          [Op.eq]: 2,
-        },
-      },
       include: agent,
     });
     res.status(200).json({
