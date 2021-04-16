e0ab8d9e859d41e9b1158763d1ac80fd1bf5fa15
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-16 19:56:35 +0300

    Poste controller created successfully

---
+++ controller/poste_controller.js
@@ -0,0 +1,25 @@
+import Poste from "../models/poste_model";
+
+const posteController = {
+  // Handling get-request for all postes
+  getPostes: async (req, res) => {
+    let response = await Poste.findAll().catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        status: 500,
+        error: {
+          message: err.message,
+        },
+      });
+    });
+    if (response) {
+      return res.status(200).json({
+        status: 200,
+        length: response.length,
+        response: response,
+      });
+    }
+  },
+};
+
+export default posteController;
