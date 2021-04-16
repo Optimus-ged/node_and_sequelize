1f5f66ed3ad240df5d0e63aac696fbe5759d83b0
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-16 19:01:09 +0300

    Agent and poste components deleted for repetition

--- routers/principle/routers_index.js
+++ routers/principle/routers_index.js
@@ -2,17 +2,12 @@
 import express from 'express';
 import productRoutes from '../product_routes';
 import userRoutes from '../user_routes';
-import posteRoutes from "../poste_routes";
-import agentRoutes from "../agent_routes";
 
 const router = express.Router();
 
 // All routes
 router
   .use("/products", productRoutes)
-  .use("/users", userRoutes)
-  .use("/postes", posteRoutes)
-  .use("/agents", agentRoutes);
+  .use("/users", userRoutes);
 
-// Exporting module
 export default router;
