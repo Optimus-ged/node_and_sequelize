a541af78177cc68d78aae7980e18d6299539cd49
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-19 20:52:59 +0300

    Error in agents routes fixed successfully

--- routers/agent_routes.js
+++ routers/agent_routes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import agentController from "../controller/agent_controller";
-
 const router = express.Router();
 
 router
-  .get("/all", agentController.getAgents)
-  .get("/one/:id", agentController.getOneAgent)
-  .get("/name/:name", agentController.getAgentByName);
+  .get("/", agentController.getAgents)
+  .get("/:id", agentController.getOneAgent)
+  .get("/agent_by_name/:name", agentController.getAgentByName);
 
 export default router;
