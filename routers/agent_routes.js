103094f31fc514751b2962e88d2501f7f0eb5982
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-19 20:59:45 +0300

    Check authenification middleware added for agent controller

--- routers/agent_routes.js
+++ routers/agent_routes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import checkAuth from "../middleware/token";
 import agentController from "../controller/agent_controller";
 const router = express.Router();
 
 router
-  .get("/", agentController.getAgents)
-  .get("/:id", agentController.getOneAgent)
-  .get("/agent_by_name/:name", agentController.getAgentByName);
+  .get("/", checkAuth, agentController.getAgents)
+  .get("/:id", checkAuth, agentController.getOneAgent)
+  .get("/agent_by_name/:name", checkAuth, agentController.getAgentByName);
 
 export default router;
