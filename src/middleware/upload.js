eac755e86b314f1f0ce12a67064fece112838ae3
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-06 21:54:25 +0300

    Repetition on upload middleware maded fore more comprehension

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,45 +1,33 @@
-// Importing dependancy
-import multer from "multer";
+// Importing depedancies
+import multer from 'multer';
 
-// function to filter my images, accept or reject some images
-// extensions
-// cb => means call back function
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+// Filtering the file
+const fileFilter = (req, file, cb)=>{
+  if(file.mimetype === "image/png" || file.mimetype === "image/jpeg"){
     cb(null, true);
-  } else {
+  }else{
     cb(null, false);
   }
-};
+}
 
-// Definition of the storage : destination and fileName
+// Defining file destinaion
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // let imgdest = "src/public/images";
-    // let dest = fs.existsSync(imgdest)
-    //   ? imgdest
-    //   : fs.mkdir(imgdest, (e) => {
-    //       if (e) throw e;
-    //     });
+  destination : (req, file, cb)=>{
     cb(null, "src/public/images");
   },
-  filename: (req, file, cb) => {
-    let dt = new Date();
-    let newName = `${dt.getUTCFullYear()}-${dt.getUTCMonth() + 1}-${dt.getUTCDate()} ${
-      file.originalname
-    }`;
-    cb(null, newName);
-  },
+  filename : (req, file, cb)=>{
+    cb(null, file.originalname)
+  }
 });
 
-// with this constant i will be able to upload the image
-// using my post-request
+// Uploading file
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
+  storage : storage,
+  limits : {
+    fileSize : 1024 * 1024 * 5
   },
-  fileFilter: fileFilter,
+  fileFilter : fileFilter
 });
 
+// Exporting module
 export default upload;
